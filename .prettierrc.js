18aefcfce09d6a02345aa558d673a1e3ca1e7d18
Raptortilla <martinrivelle@gmail.com>
2023-12-30 21:36:34 -0300

    Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'all',
+  useTabs: false, // Use tabulations
+  tabWidth: 2, // Tab spaces
+  semi: true, // Force semicolons
+  singleQuote: true, // Use single quotes for strings
+  printWidth: 100, // Row width
+  arrowParens: 'always', // Parenthesis for arrow functions
+};
