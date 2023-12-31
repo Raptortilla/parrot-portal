54fbb8f4cff8c6814637f5629b546a6e0cf1b7e6
Martin Rivelle <martinrivelle@gmail.com>
2023-12-30 22:01:03 -0300

    Prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,6 +4,6 @@ module.exports = {
   tabWidth: 2, // Tab spaces
   semi: true, // Force semicolons
   singleQuote: true, // Use single quotes for strings
-  printWidth: 100, // Row width
+  printWidth: 80, // Row width
   arrowParens: 'always', // Parenthesis for arrow functions
 };
