c696c60fd08c40bd1209f87e79b9a38352a7b450
Raptortilla <martinrivelle@gmail.com>
2024-01-01 14:11:02 -0300

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import LoginPage from './pages/Login';
 import HomePage from './pages/Home';
 
 function App() {
+  console.log(process.env.COGNITO_USER_POOL_ID);
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
